70da3b28e9b64934d71a89b7236c8814e0e9cf0c
bmistree <bmistree@gmail.com>
2014-06-29 17:49:28 -0700

    Added slow logic for determining if one header is a superset of another.

--- lib/header.cpp
+++ lib/header.cpp
@@ -1,11 +1,31 @@
 #include "header.hpp"
 #include <string>
+#include <cassert>
 
 Header::Header(std::string header_str)
  : _header_str(header_str)
 {}
 Header::~Header()
 {}
+
+bool Header::superset(const Header& h) const
+{
+    //Returns true if h is a superset of this.  False otherwise.
+    if (h._header_str.size() != _header_str.size())
+        assert(false);
+    
+    for (size_t i =0; i < h._header_str.size(); ++i)
+    {
+        char h_char = h._header_str[i];
+        if (h_char == MATCH_ANY_CHAR)
+            continue;
+
+        char m_char = _header_str[i];
+        if (m_char != h_char)
+            return false;
+    }
+    return true;
+}
 
 const std::string& Header::header_str() const
 {
--- lib/header.hpp
+++ lib/header.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 
+#define MATCH_ANY_CHAR 'x'
+
 /**
    Currently, tracking header contents with simple string.  String can only have
    1s, 0s, and xs.  xs mean "match any".
@@ -14,6 +16,11 @@ public:
     Header(std::string header_str);
     ~Header();
     const std::string& header_str() const;
+
+    /**
+       @returns Returns true if h is a superset of this.  False otherwise.
+     */
+    bool superset(const Header& h) const;
     
 private:
     std::string _header_str;
