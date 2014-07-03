ea81d337b3c9ce08cf3ecbc4e050509b9b8bb5d3
bmistree <bmistree@gmail.com>
2014-07-02 23:21:57 -0700

    Added a test for priority updates.

---
+++ test/priority_random.cpp
@@ -0,0 +1,42 @@
+#include "../lib/header.hpp"
+#include "../lib/table.hpp"
+
+#include <iomanip>
+#include <iostream>
+
+
+int main(int argc, char**argv)
+{
+    Table table;
+    Entry* entry_a =
+        new Entry(
+            2000,Header("1x111"));
+    UniqueEntryPtr entry_ptr_a(entry_a);
+    table.add_entry(entry_ptr_a);
+
+    Entry* entry_b =
+        new Entry(
+            1999,Header("0xxx1"));
+    UniqueEntryPtr entry_ptr_b(entry_b);
+    table.add_entry(entry_ptr_b);
+
+    Entry* entry_c =
+        new Entry(
+            1998,Header("xx011"));
+    UniqueEntryPtr entry_ptr_c(entry_c);
+    table.add_entry(entry_ptr_c);
+
+    Entry* entry_d =
+        new Entry(
+            1997,Header("000xx"));
+    UniqueEntryPtr entry_ptr_d(entry_d);
+    table.add_entry(entry_ptr_d);
+
+    // sorts by priority
+    table.finalize();
+
+    for (int i = 0; i < 5; ++i)
+        table.priority_random();
+    table.debug_print_table();
+    return 0;
+}
